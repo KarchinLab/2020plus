6376b827cb40941ad329de0f4045493a50807c16
ctokheim <collintokheim@gmail.com>
2013-10-14 14:56:46 -0400

    Changed AminoAcid class to read HGVS style syntax. Also added unit tests to evaluate AminoAcid.

---
+++ tests/test_amino_acid.py
@@ -0,0 +1,100 @@
+from ..utils.python.amino_acid import AminoAcid
+
+def test_missense_mutation():
+    """Tests if AminoAcid properly parses missense HGVS syntax."""
+    # test strings
+    missense1 = 'p.A391E'
+    missense2 = 'a391e'
+
+    # case 1
+    aa = AminoAcid(missense1)
+    assert aa.is_missense, 'Did not correctly detect a missense mutation'
+    assert aa.initial == 'A', 'Did not correctly parse initial AA'
+    assert aa.pos == 391, 'Did not correctly parse missense location'
+    assert aa.mutated == 'E', 'Did not correctly parse mutated AA'
+
+    # case 2
+    aa.set_amino_acid(missense2)
+    assert aa.is_missense, 'Likely issue with parsing non-standard HGVS'
+    assert aa.initial == 'A', 'Likely issue with parsing non-standard HGVS'
+    assert aa.pos == 391, 'Likely issue with parsing non-standard HGVS'
+    assert aa.mutated == 'E', 'Likely issue with parsing non-standard HGVS'
+
+def test_insertion_mutation():
+    """Tests if AminoAcid properly parses insertions using HGVS syntax."""
+    # test strings
+    ins1 = 'p.L601_K602ins27'
+    ins2 = 'p.k313_v314insS'
+    ins3 = 'p.?_?ins?'
+
+    # case 1 -- num of inserted bases
+    aa = AminoAcid(ins1)
+    assert aa.is_insertion, 'Did not detect an insertion.'
+    assert aa.initial == ['L', 'K'], 'Initial AA: ["L", "K"] != ' + str(aa.initial)
+    assert aa.pos == [601, 602], 'Insertion pos: [601, 602] != ' + str(aa.pos)
+    assert aa.mutated == '27', 'Did not correctly parse inserstion mutation'
+
+    # case 2 -- inserted base info
+    aa = AminoAcid(ins2)
+    assert aa.is_insertion, 'Did not detect an insertion.'
+    assert aa.initial == ['K', 'V'], 'Uncorrectly handled lower case.'
+    assert aa.pos == [313, 314], 'Did not correctly parse insertion position'
+    assert aa.mutated == 'S', 'Did not correctly parse inserstion mutation'
+
+    # case 3 -- mising information
+    aa = AminoAcid(ins3)
+    assert aa.is_missing_info, 'Problem with detecting missing information.'
+    assert aa.is_insertion, 'Problem with missing values.'
+    assert aa.initial == '', 'Problem with missing values.'
+    assert aa.pos == [], 'Problem with missing values.'
+    assert aa.mutated == '', 'Problem with missing values.'
+
+def test_deletion_mutation():
+    """Tests if AminoAcid properly parses deletions using HGVS syntax."""
+    # test strings
+    del1 = 'p.S16_Q20del'
+    del2 = 'p.?del'
+    del3 = 'p.S45del'
+    del4 = 'p.T155_A161delTRVRAMA'
+
+    # case 1 -- normal
+    aa = AminoAcid(del1)
+    assert aa.is_deletion, 'Problem with detecting deletions.'
+    assert aa.initial == ['S', 'Q'], 'Problem with parsing initial AA.'
+    assert aa.pos == [16, 20], 'Problem with parsing deletion positon.'
+    assert aa.mutated == '', 'Problem with parsing self.mutated'
+
+    # case 2 -- missing info
+    aa = AminoAcid(del2)
+    assert aa.is_missing_info, 'Problem detecting missing information'
+
+    # case 3 -- single base deletion
+    aa = AminoAcid(del3)
+    assert aa.initial == ['S'], 'Problem parsing single bp deletion'
+    assert aa.pos == [45], 'Problem parsing single bp deletion'
+
+    # case 4 -- deleted base information
+    aa = AminoAcid(del4)
+    assert aa.mutated == 'TRVRAMA', 'Did not record deleted base information'
+
+def test_frame_shift_mutation():
+    """Tests if AminoAcid properly parses frame shifts using HGVS syntax."""
+    # test strings
+    fs1 = 'p.M1fs'
+    fs2 = 'p.W288fs*12'
+
+    # case 1 -- frame shift without premature stop codon
+    aa = AminoAcid(fs1)
+    assert aa.is_frame_shift, 'Problem with detecting frame shift events.'
+    assert aa.initial == 'M'
+    assert aa.mutated == ''
+    assert aa.pos == 1
+    assert aa.stop_pos == None
+
+    # case 2 -- frame shift with premature stop codon
+    aa = AminoAcid(fs2)
+    assert aa.initial == 'W'
+    assert aa.pos == 288
+    assert aa.stop_pos == 12
+
+
--- utils/python/amino_acid.py
+++ utils/python/amino_acid.py
@@ -2,42 +2,200 @@ import re
 
 class AminoAcid(object):
     """ The AminoAcid class represents aa changes in the Cosmic Database.
+
+    The AminoAcid class follows the syntax of HGVS
+    (http://www.hgvs.org/mutnomen/recs-prot.html). Although the parsing
+    generally follows the HGVS syntax, it does have slight variations where
+    the COSMIC database uses idiosyncratic conventions.
+
+    The AminoAcid class is only intended to be used in the following way::
+
+        >>> aa = AminoAcid('p.A267C')
+        >>> aa.pos
+        267
+        >>> aa.is_missense
+        True
+
+    Namely, the constructor parses the necessary HGVS string and then extracts
+    attributes that can be used.
     """
 
-    def __init__(self, amino_acid='', occurrence=0):
-        if re.search('^[A-Za-z]\d+[A-Za-z]$', amino_acid):
-            self.set_amino_acid(amino_acid)
-            if occurrence:
-                self.occurrence = occurrence
-            self.is_valid = True
-        else:
-            self.is_valid = False
+    def __init__(self, hgvs='', occurrence=1):
+        self.hgvs_original = hgvs
+        hgvs = hgvs.upper()  # convert everything to upper case
+        self.hgvs = hgvs
+        self.occurrence = occurrence
+        self.set_amino_acid(hgvs)
 
     def set_occurrence(self, occur):
         self.occurrence = occur
 
     def set_amino_acid(self, aa):
         """Set amino acid change and position."""
-        self.initial, self.pos, self.mutated = self.__read_amino_acid_change(aa)
+        aa = aa.upper()  # make sure it is upper case
+        aa = aa[2:] if aa.startswith('P.') else aa  # strip "p."
+        self.__set_mutation_status()  # set flags detailing the type of mutation
+        self.__read_hgvs_syntax(aa)  # read in specific mutations
 
-    def __read_amino_acid_change(self, aa_change):
-        """Convert A207T syntax for amino acid change to tuple ("A", 207, "T").
+    def __set_mutation_status(self):
+        # strip "p." from HGVS protein syntax
+        hgvs_tmp = self.hgvs[2:] if self.hgvs.startswith("P.") else self.hgvs
+
+        # set evidence status
+        self.__set_unkown_effect(hgvs_tmp)  # unknown effect
+        self.__set_no_protein(hgvs_tmp)  # no protein
+        self.__set_mutation_type(hgvs_tmp)  # indel, missense, etc.
+
+    def __set_mutation_type(self, hgvs_string):
+        """Interpret the mutation type (missense, etc.) and set appropriate flags.
 
         Args:
-            aa_change (str): amino acid string ("A207T")
-
-        Returns:
-            tuple. The order::
-
-                0 -- initial aa
-                1 -- aa position
-                2 -- mutated aa
+            hgvs_string (str): hgvs syntax with "p." removed
         """
-        try:
-            initial_aa = aa_change[0].upper()  # starting aa
-            pos = int(aa_change[1:-1])
-            mutated_aa = aa_change[-1].upper()  # mutated aa
-        except:
-            print aa_change
-            raise
-        return initial_aa, pos, mutated_aa
+        self.__set_missense_status(hgvs_string)  # missense mutations
+        self.__set_indel_status()  # indel mutations
+        self.__set_frame_shift_status()  # check for fs
+        self.__set_premature_stop_codon_status(hgvs_string)  # check for stops
+
+    def __set_missense_status(self, hgvs_string):
+        """Sets the self.is_missense flag."""
+        # set missense status
+        if re.search('^[A-Z?]\d+[A-Z?]$', hgvs_string):
+            self.is_missense = True
+        else:
+            self.is_missense = False
+
+    def __set_frame_shift_status(self):
+        """Check for frame shift and set the self.is_frame_shift flag."""
+        if 'fs' in self.hgvs_original:
+            self.is_frame_shift = True
+        else:
+            self.is_frame_shift = False
+
+    def __set_premature_stop_codon_status(self, hgvs_string):
+        """Set whether there is a premature stop codon."""
+        if re.search('.+\*(\d+)?$', hgvs_string):
+            self.is_premature_stop_codon = True
+
+            # check if it is also a nonsense mutation
+            if hgvs_string.endswith('*'):
+                self.is_nonsense_mutation = True
+            else:
+                self.is_nonsense_mutation = False
+        else:
+            self.is_premature_stop_codon = False
+            self.is_nonsense_mutation = False
+
+    def __set_indel_status(self):
+        """Sets flags related to the mutation being an indel."""
+        # set indel status
+        if "ins" in self.hgvs_original:
+            # mutation is insertion
+            self.is_insertion = True
+            self.is_deletion = False
+            self.is_indel = True
+        elif "del" in self.hgvs_original:
+            # mutation is deletion
+            self.is_deletion = True
+            self.is_insertion = False
+            self.is_indel = True
+        else:
+            # not an indel
+            self.is_deletion = False
+            self.is_insertion = False
+            self.is_indel = False
+
+
+    def __set_unkown_effect(self, hgvs_string):
+        """Sets a flag for unkown effect according to HGVS syntax. The
+        COSMIC database also uses unconventional questionmarks to denote
+        missing information.
+
+        Args:
+            hgvs_string (str): hgvs syntax with "p." removed
+        """
+        # Standard use by HGVS of indicating unknown effect.
+        unknown_effect_list = ['?', '(=)', '=']  # unknown effect symbols
+        if hgvs_string in unknown_effect_list:
+            self.unknown_effect = True
+        elif "(" in hgvs_string:
+            # parethesis in HGVS indicate expected outcomes
+            self.unknown_effect = True
+        else:
+            self.unknown_effect = False
+
+        # detect if there are missing information. commonly COSMIC will
+        # have insertions with p.?_?ins? or deleteions with ?del indicating
+        # missing information.
+        if "?" in hgvs_string:
+            self.is_missing_info = True
+        else:
+            self.is_missing_info = False
+
+    def __set_no_protein(self, hgvs_string):
+        """Set a flag for no protein expected.
+
+        Args:
+            hgvs_string (str): hgvs syntax with "p." removed
+        """
+        no_protein_list = ['0', '0?']  # no protein symbols
+        if hgvs_string in no_protein_list:
+            self.no_protein = True
+        else:
+            self.no_protein = False
+
+
+    def __read_hgvs_syntax(self, aa_hgvs):
+        """Convert HGVS syntax for amino acid change into attributes.
+
+        Specific details of the mutation are stored in attributes like
+        self.intial (prior to mutation), sel.pos (mutation position),
+        self.mutated (mutation), and self.stop_pos (position of stop codon,
+        if any).
+
+        Args:
+            aa_hgvs (str): amino acid string following HGVS syntax
+        """
+        if self.is_missense:
+            self.initial = aa_hgvs[0]
+            self.mutated = aa_hgvs[-1]
+            self.pos = int(aa_hgvs[1:-1])
+            self.stop_pos = None  # not a nonsense mutation
+            if self.initial == self.mutated:
+                self.is_synonymous = True
+            else:
+                self.is_synonymous = False
+        elif self.is_indel:
+            if self.is_insertion:
+                if not self.is_missing_info:
+                    self.initial = re.findall('([A-Z])\d+', aa_hgvs)[:2]  # first two
+                    self.pos = map(int, re.findall('[A-Z](\d+)', aa_hgvs)[:2])  # first two
+                    self.mutated = re.findall('(?<=INS)[A-Z0-9?]+', aa_hgvs)[0]
+                    self.mutated = self.mutated.strip('?')  # remove the missing info '?'
+                else:
+                    self.initial = ''
+                    self.pos = []
+                    self.mutated = ''
+            elif self.is_deletion:
+                if not self.is_missing_info:
+                    self.initial = re.findall('([A-Z])\d+', aa_hgvs)
+                    self.pos = map(int, re.findall('[A-Z](\d+)', aa_hgvs))
+                    self.mutated = re.findall('(?<=DEL)[A-Z]*', aa_hgvs)[0]
+                else:
+                    self.initial = ''
+                    self.pos = []
+                    self.mutated = ''
+        elif self.is_nonsense_mutation:
+            self.initial = aa_hgvs[0]
+            self.mutated = ''  # there is actually a stop codon
+            self.stop_pos = 0  # indicates same position is stop codon
+            self.pos = aa_hgvs[1:-1]
+        elif self.is_frame_shift:
+            self.initial = aa_hgvs[0]
+            self.mutated = ''
+            self.pos = int(re.findall('[A-Z](\d+)', aa_hgvs)[0])
+            if self.is_premature_stop_codon:
+                self.stop_pos = int(re.findall('\*>?(\d+)$', aa_hgvs)[0])
+            else:
+                self.stop_pos = None
+
