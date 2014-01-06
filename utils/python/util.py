bd85b3bb0a272db83e52ff50e2752c5676ee70cf
ctokheim <collintokheim@gmail.com>
2014-01-06 12:50:53 -0500

    Added olfactory gene list reader

--- utils/python/util.py
+++ utils/python/util.py
@@ -28,11 +28,15 @@ clf_result_dir = _opts['clf_result_dir']
 def read_aa_properties(file_path):
     """Read aa property counts from the data_analysis/results folder.
 
-    Args:
-        file_path (str): path to aa_change.properties.txt
+    **Parameters**
 
-    Returns:
-        DataFrame. contains mutation counts for amino acid chemical properties
+    file_path : str
+        path to aa_change.properties.txt
+
+    **Returns**
+
+    df : pd.DataFrame
+        contains mutation counts for amino acid chemical properties
     """
     logger = logging.getLogger(name=__name__)
     logger.info('reading in %s ...' % file_path)
@@ -48,8 +52,10 @@ def read_oncogenes():
     Oncogenes from supplementary 2A:
     http://www.sciencemag.org/content/339/6127/1546.full
 
-    Returns:
-        oncogenes (tuple): tuple of gene names considered oncogenes
+    **Returns**
+
+    oncogenes : tuple
+        tuple of gene names considered oncogenes
     """
     cfg_opts = get_input_config('input')
     with open(cfg_opts['oncogene'], 'r') as handle:
@@ -63,8 +69,10 @@ def read_tsgs():
     Oncogenes from supplementary 2A:
     http://www.sciencemag.org/content/339/6127/1546.full
 
-    Returns:
-        tsgs (tuple): tuple of gene names considered as tumor suppressors
+    **Returns**
+
+    tsgs : tuple
+        tuple of gene names considered as tumor suppressors
     """
     cfg_opts = get_input_config('input')
     with open(cfg_opts['tsg'], 'r') as handle:
@@ -81,8 +89,10 @@ def read_smg():
     SMGs from supplementary:
     http://www.nature.com/nature/journal/v502/n7471/full/nature12634.html
 
-    Returns:
-        smgs (tuple): tuple of gene names considered as significantly mutated
+    **Returns**
+
+    smgs : tuple
+        tuple of gene names considered as significantly mutated
     """
     cfg_opts = get_input_config('input')
     with open(cfg_opts['smg'], 'r') as handle:
@@ -105,14 +115,47 @@ def read_smg():
     return smgs_in_database
 
 
+def read_olfactory_receptors():
+    """Reads in the significant olfactory receptors from Mutsigcv.
+
+    **Returns**
+
+    olfactory : tuple
+        tuple of gene names considered as olfactory receptors
+    """
+    cfg_opts = get_input_config('input')
+    with open(cfg_opts['olfactory_receptors'], 'r') as handle:
+        olfactory = tuple(gene.strip() for gene in handle.readlines())
+
+    # open connection
+    gene_db_path = get_db_config('genes')['db']
+    conn = sqlite3.connect(gene_db_path)
+
+    sql = ("SELECT DISTINCT Gene"
+          " FROM nucleotide"
+          " WHERE Gene in " + str(olfactory))
+
+    df = psql.frame_query(sql, con=conn)
+    conn.close()  # close connection
+
+    # get significantly mutated genes found in database
+    olfactory_in_database = tuple(df['Gene'])
+    logger.info('There are only %d/%d olfactory receptors found in the database.'
+                % (len(olfactory_in_database), len(olfactory)))
+    return olfactory_in_database
+
+
 def classify_gene(gene):
     """Return whether the gene is an oncogene, tsg, or other.
 
-    Args:
-        gene (str): Official gene name
+    **Parameters**
 
-    Returns:
-        Str: 'oncogene', 'tsg', or 'other'
+    gene : str
+        Official gene name
+
+    **Returns**
+
+        Str, ['oncogene' | 'tsg' | 'other']
     """
     if gene in oncogene_set:
         return 'oncogene'
@@ -125,11 +168,15 @@ def classify_gene(gene):
 def get_mutation_types(hgvs_iterable, kind='amino acid'):
     """Classify each protein HGVS mutation as a certain type.
 
-    Args:
-        hgvs_iterable (iterable): iterable container with HGVS mutaiton strings
+    **Parameters**
 
-    Returns:
-        pd.Series: container of protein mutation types in same order as input
+    hgvs_iterable : iterable
+        iterable container with HGVS mutaiton strings
+
+    **Returns**
+
+    mut_type_series : pd.Series
+        container of protein mutation types in same order as input
     """
     mut_type = []
     if kind == 'amino acid':
@@ -148,11 +195,15 @@ def count_mutation_types(hgvs_iterable, kind='amino acid'):
     """Count mutation types from HGVS protein strings (missense, indels, etc.)
     and DNA strings (substitutions, indels).
 
-    Args:
-        hgvs_iterable (iterable): An iterable object containing protein HGVS
+    **Parameters**
 
-    Returns:
-        pd.Series: A pandas series object counting protein mutation types
+    hgvs_iterable : iterable
+        An iterable object containing protein HGVS
+
+    **Returns**
+
+    unique_cts : pd.Series
+        A pandas series object counting protein mutation types
     """
     mut_type_series = get_mutation_types(hgvs_iterable, kind=kind)  # get mutation types
     unique_cts = mut_type_series.value_counts() # count mutation types
@@ -181,11 +232,15 @@ def read_cosmic_tsv_by_gene(gene_name):
     NOTE: Assumes cosmic flat files are in cosmic_dir specified by input.cfg
     and are sorted into alphabetical directories (eg. 'A'...'Z').
 
-    Args:
-        gene_name (str): gene name
+    **Parameters**
 
-    Returns:
-        pd.DataFrame: tsv file as a pandas dataframe
+    gene_name : str
+        gene name
+
+    **Returns**
+
+    df : pd.DataFrame
+        tsv file as a pandas dataframe
     """
     cfg_opt = get_input_config('input')
     database_dir = cfg_opt['cosmic_dir']  # COSMIC_nuc database directory
@@ -199,17 +254,18 @@ def drop_table(tbl_name,
                kind='sqlite'):
     """Drop a table from database if exists.
 
-    Note: This function was written because pandas has a bug.
+    **Note:** This function was written because pandas has a bug.
     If pandas was working then the write_frame method could just
     replace existing contents with out the need for me to drop the
     table. The bug is found here:
         https://github.com/pydata/pandas/issues/2971
 
-    Args:
-        tbl_name (str): name of table to drop
+    **Parameters**
 
-    Kwargs:
-        kind (str): ['sqlite' | 'mysql']
+    tbl_name : str
+        name of table to drop
+    kind : str, ['sqlite' | 'mysql']
+        type of database
     """
     genes_db_path = get_db_config('genes')['db']
     if kind == 'sqlite':
@@ -231,3 +287,7 @@ tsg_set = set(tsg_list)
 # significantly mutate genes from kandoth et al
 smg_list = read_smg()
 smg_set = set(smg_list)
+
+# olfactory receptors from mutsigcv
+olfactory_list = read_olfactory_receptors()
+olfactory_set = set(olfactory_list)
