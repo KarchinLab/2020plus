d24d4416e1aa15c80d5e633154882a000ff4d275
ctokheim <collintokheim@gmail.com>
2014-08-22 10:51:06 -0400

    Added code for simulating consistency

---
+++ src/simulation/python/random_split.py
@@ -0,0 +1,144 @@
+import numpy as np
+import pandas as pd
+import pandas.io.sql as psql
+import src.data_analysis.python.feature_matrix as fmat
+import src.data_analysis.python.position_entropy as pentropy
+import src.features.python.features as feat
+import logging
+
+logger = logging.getLogger(__name__)
+
+class RandomSplit(object):
+
+    def __init__(self, sub_sample,
+                 num_iter,
+                 db_conn,
+                 table_name='mutation',
+                 col_name='Tumor_Sample'):
+        self.db_conn = db_conn
+        self.set_sub_sample(sub_sample)
+        self.set_num_iter(num_iter)
+        self.TABLE_NAME = table_name
+        self.COLUMN_NAME = col_name
+        self.set_df(None)
+
+    def dataframe_generator(self):
+        """Generate subsampled data frames according to the sub_sample
+        and num_iter arguments. The tumor type composition is respect, i.e.
+        the relative amount of each tumor type for a specific sample will
+        always reflect the relative amount in aggregate.
+        """
+        #n = int(self.sub_sample * self.total_count)  # number of counts to sample
+
+        for i in range(self.num_iter):
+            logger.info('Feature generation: Sub-sample rate={0}, Iteration={1} . . .'.format(self.sub_sample, i))
+            left_samples = []
+            right_samples = []
+
+            # randomly split sample names while respecting tumor type
+            # composition
+            prng = np.random.RandomState()
+            for tmp_ttype, tmp_samples in self.sample_names.iteritems():
+                tmp_num_samps = len(tmp_samples)
+                prng.shuffle(tmp_samples)  # shuffle order of samples
+                split_pos = int(tmp_num_samps*self.sub_sample)
+                tmp_left = tmp_samples[:split_pos]
+                tmp_right = tmp_samples[split_pos:]
+                left_samples += tmp_left
+                right_samples += tmp_right
+
+            # process features with newly defined tumor samples to use
+            left_feat_df = self._process_features(left_samples)
+            right_feat_df = self._process_features(right_samples)
+
+            logger.info('Finished feature generation: Sub-sample rate={0}, Iteration={1}'.format(self.sub_sample, i))
+            yield left_feat_df, right_feat_df
+
+    def _process_features(self, samps_of_interest):
+        """Processes features from only the specified tumor samples of interest.
+
+        Parameters
+        ----------
+        samps_of_interest: list/set
+            list/set of tumor sample id's to use for generating features
+
+        Returns
+        -------
+        proc_feat_df: pd.DataFrame
+            dataframe consisting of features for classification
+        """
+        # make sure samples are a set object to speed up computation
+        samps_of_interest = set(samps_of_interest)
+
+        # get data from those sample names
+        samp_flag = self.df[self.COLUMN_NAME].apply(lambda x: x in samps_of_interest)
+        ixs = samp_flag[samp_flag==True].index
+        tmp_df = self.df.ix[ixs].copy()
+
+        # process features
+        feat_list = fmat.generate_feature_matrix(tmp_df, 2)
+        headers = feat_list.pop(0)  # remove header row
+        feat_df = pd.DataFrame(feat_list, columns=headers)  # convert to data frame
+        proc_feat_df = feat.process_features(feat_df, 0)
+        miss_ent_df = pentropy.missense_position_entropy(tmp_df[['Gene', 'AminoAcid']])
+        mut_ent_df = pentropy.mutation_position_entropy(tmp_df[['Gene', 'AminoAcid']])
+
+        # encorporate entropy features
+        proc_feat_df['mutation position entropy'] = mut_ent_df['mutation position entropy']
+        proc_feat_df['pct of uniform mutation entropy'] = mut_ent_df['pct of uniform mutation entropy']
+        proc_feat_df['missense position entropy'] = miss_ent_df['missense position entropy']
+        proc_feat_df['pct of uniform missense entropy'] = miss_ent_df['pct of uniform missense entropy']
+        return proc_feat_df
+
+    def set_sub_sample(self, sub_sample):
+        """Set the fraction of the original total mutations to actually sample.
+
+        Sampling is done without replacement.
+
+        **Parameters**
+
+        sub_sample : float
+            0 < sub_sample <= 1.0
+        """
+        if 0 <= sub_sample <= 1:
+            self.sub_sample = sub_sample
+        else:
+            raise ValueError('Subsample should be between zero and one.')
+
+    def set_num_iter(self, num_iter):
+        """Set number of times to sample w/o replacement.
+
+        **Parameters**
+
+        num_iter : int
+            do sample w/o replacement, num_iter number of times
+        """
+        if iter > 0:
+            self.num_iter = num_iter
+        else:
+            raise ValueError('Number of iterations should be positive.')
+
+    def set_df(self, df):
+        if df:
+            self.df = df
+        else:
+            sql = ("SELECT Gene, Protein_Change as AminoAcid, "
+                   "       DNA_Change as Nucleotide, "
+                   "       Variant_Classification, "
+                   "       Tumor_Sample, Tumor_Type "
+                   "FROM {0}".format(self.TABLE_NAME))
+            self.df = psql.frame_query(sql, con=self.db_conn)
+        drop_dups = self.df[['Tumor_Sample', 'Tumor_Type']].drop_duplicates()
+        self.sample_names = drop_dups.set_index('Tumor_Sample').groupby('Tumor_Type').groups
+        # self.sample_names = self.df[self.COLUMN_NAME].unique()
+
+        # check validaty of data
+        if not np.max(drop_dups['Tumor_Sample'].value_counts()) > 1:
+            # each tumor sample has only one associated tumor type
+            self.num_sample_names = len(self.sample_names)
+        else:
+            # yikes there are more than one tumor type associated with this sample
+            # raise ValueError('A tumor sample has more than one tumor type!')
+            pass
+
+        self.total_count = len(self.df)
---
+++ src/simulation/python/simulate_consistency.py
@@ -0,0 +1,380 @@
+from src.classify.python.r_random_forest_clf import RRandomForest
+from src.classify.python.random_forest_clf import RandomForest
+from src.classify.python.multinomial_nb_clf import MultinomialNaiveBayes
+import src.utils.python.util as _utils
+import simulate_performance as sp
+from random_split import RandomSplit
+import pandas as pd
+import scipy.stats as stats
+import sqlite3
+from multiprocessing import Pool
+import itertools as it
+import time
+
+
+def rank_genes(s1, s2,
+               mask1=None, mask2=None,
+               thresh=None):
+    #s1.sort(ascending=False)
+    #s2.sort(ascending=False)
+    # use score threshold if provided
+    if thresh:
+        all_ixs = list(set(s1[s1>thresh].index) | set(s2[s2>thresh].index))
+    elif mask1 is not None and mask2 is not None:
+        all_ixs = list(set(s1[mask1].index) | set(s2[mask2].index))
+    else:
+        all_ixs = list(set(s1.index) | set(s2.index))
+
+    top_s1 = s1[all_ixs]
+    top_s2 = s2[all_ixs]
+    top_rank1 = top_s1.fillna(0).rank()
+    top_rank2 = top_s2.fillna(0).rank()
+    top_rank2 = top_rank2[top_rank1.index]  # match ordering of index
+    return top_rank1, top_rank2
+
+
+def jaccard_index(s1, s2,
+                  mask1=None, mask2=None,
+                  thresh=None):
+    if thresh:
+        s1, s2 = s1[s1>thresh], s2[s2>thresh]
+    elif mask1 is not None and mask2 is not None:
+        s1, s2 = s1[mask1], s2[mask2]
+    s1_genes = set(s1.index)
+    s2_genes = set(s2.index)
+    num_intersect = len(s1_genes & s2_genes)
+    num_union = len(s1_genes | s2_genes)
+    if num_union:
+        # provided series are not empty
+        jaccard_sim = num_intersect / float(num_union)
+    else:
+        # empty series case
+        jaccard_sim = 0
+    return jaccard_sim
+
+
+def r_random_forest_compare(df1, df2, opts):
+    # predict and compile results
+    rrclf1 = RRandomForest(df1,
+                           other_sample_ratio=opts['other_ratio'],
+                           driver_sample=opts['driver_rate'],
+                           ntrees=opts['ntrees'],
+                           min_ct=opts['min_count'])
+    onco_prob1, tsg_prob1, other_prob1 = rrclf1.kfold_prediction()
+    driver_prob1 = onco_prob1 + tsg_prob1
+    df1 = pd.DataFrame({'onco': onco_prob1,
+                        'tsg': tsg_prob1,
+                        'other': other_prob1,
+                        'driver': driver_prob1},
+                       index=onco_prob1.index)
+    rrclf2 = RRandomForest(df2,
+                           other_sample_ratio=opts['other_ratio'],
+                           driver_sample=opts['driver_rate'],
+                           ntrees=opts['ntrees'],
+                           min_ct=opts['min_count'])
+    onco_prob2, tsg_prob2, other_prob2 = rrclf2.kfold_prediction()
+    driver_prob2 = onco_prob2 + tsg_prob2
+    df2 = pd.DataFrame({'onco': onco_prob2,
+                        'tsg': tsg_prob2,
+                        'other': other_prob2,
+                        'driver': driver_prob2},
+                       index=onco_prob2.index)
+    # get predicted class
+    onco_pred1 = (df1['onco'] > df1['tsg']) & (df1['onco'] > df1['other'])
+    onco_pred2 = (df2['onco'] > df2['tsg']) & (df2['onco'] > df2['other'])
+    tsg_pred1 = (df1['tsg'] > df1['onco']) & (df1['tsg'] > df1['other'])
+    tsg_pred2 = (df2['tsg'] > df2['onco']) & (df2['tsg'] > df2['other'])
+
+    # rank genes
+    driver_top_rank1, driver_top_rank2 = rank_genes(driver_prob1, driver_prob2, thresh=.5)
+    onco_top_rank1, onco_top_rank2 = rank_genes(onco_prob1, onco_prob2,
+                                                mask1=onco_pred1, mask2=onco_pred2)
+    tsg_top_rank1, tsg_top_rank2 = rank_genes(tsg_prob1, tsg_prob2,
+                                              mask1=tsg_pred1, mask2=tsg_pred2)
+
+    # calculate jaccard index
+    driver_jaccard = jaccard_index(driver_prob1, driver_prob2, thresh=.5)
+    onco_jaccard = jaccard_index(onco_prob1, onco_prob2,
+                                 mask1=onco_pred1, mask2=onco_pred2)
+    tsg_jaccard = jaccard_index(tsg_prob1, tsg_prob2,
+                                mask1=tsg_pred1, mask2=tsg_pred2)
+
+    # calc spearman rank correlation
+    sp_rho_driver, sp_pval_driver = stats.pearsonr(driver_top_rank1, driver_top_rank2)
+    sp_rho_onco, sp_pval_onco = stats.pearsonr(onco_top_rank1, onco_top_rank2)
+    sp_rho_tsg, sp_pval_tsg = stats.pearsonr(tsg_top_rank1, tsg_top_rank2)
+
+    # calc kendall tau correlation
+    kt_rho_driver, kt_pval_driver = stats.kendalltau(driver_top_rank1, driver_top_rank2)
+    kt_rho_onco, kt_pval_onco = stats.kendalltau(onco_top_rank1, onco_top_rank2)
+    kt_rho_tsg, kt_pval_tsg = stats.kendalltau(tsg_top_rank1, tsg_top_rank2)
+
+    results = pd.DataFrame({'jaccard index': [onco_jaccard,
+                                              tsg_jaccard,
+                                              driver_jaccard],
+                            'spearman correlation': [sp_rho_onco,
+                                                     sp_rho_tsg,
+                                                     sp_rho_driver],
+                            'kendall tau correlation': [kt_rho_onco,
+                                                        kt_rho_tsg,
+                                                        kt_rho_driver],
+                            'spearman p-value': [sp_pval_onco,
+                                                 sp_pval_tsg,
+                                                 sp_pval_driver],
+                            'kendall tau p-value': [kt_pval_onco,
+                                                    kt_pval_tsg,
+                                                    kt_pval_driver]},
+                            index=['oncogene', 'tsg', 'driver'])
+    return results
+
+
+def py_random_forest_compare(df1, df2, opts):
+    rclf1 = RandomForest(df1,
+                         ntrees=opts['ntrees'],
+                         min_ct=opts['min_count'])
+    onco_prob1, tsg_prob1, other_prob1 = rclf1.kfold_prediction()
+    driver_prob1 = onco_prob1 + tsg_prob1
+    df1 = pd.DataFrame({'onco': onco_prob1,
+                        'tsg': tsg_prob1,
+                        'other': other_prob1,
+                        'driver': driver_prob1},
+                       index=onco_prob1.index)
+    rclf2 = RandomForest(df2,
+                         ntrees=opts['ntrees'],
+                         min_ct=opts['min_count'])
+    onco_prob2, tsg_prob2, other_prob2 = rclf2.kfold_prediction()
+    driver_prob2 = onco_prob2 + tsg_prob2
+    df2 = pd.DataFrame({'onco': onco_prob2,
+                        'tsg': tsg_prob2,
+                        'other': other_prob2,
+                        'driver': driver_prob2},
+                       index=onco_prob2.index)
+
+    # get predicted class
+    onco_pred1 = (df1['onco'] > df1['tsg']) & (df1['onco'] > df1['other'])
+    onco_pred2 = (df2['onco'] > df2['tsg']) & (df2['onco'] > df2['other'])
+    tsg_pred1 = (df1['tsg'] > df1['onco']) & (df1['tsg'] > df1['other'])
+    tsg_pred2 = (df2['tsg'] > df2['onco']) & (df2['tsg'] > df2['other'])
+
+    # rank genes
+    driver_top_rank1, driver_top_rank2 = rank_genes(driver_prob1, driver_prob2, thresh=.5)
+    onco_top_rank1, onco_top_rank2 = rank_genes(onco_prob1, onco_prob2,
+                                                mask1=onco_pred1, mask2=onco_pred2)
+    tsg_top_rank1, tsg_top_rank2 = rank_genes(tsg_prob1, tsg_prob2,
+                                              mask1=tsg_pred1, mask2=tsg_pred2)
+
+    # calculate jaccard index
+    driver_jaccard = jaccard_index(driver_prob1, driver_prob2, thresh=.5)
+    onco_jaccard = jaccard_index(onco_prob1, onco_prob2,
+                                 mask1=onco_pred1, mask2=onco_pred2)
+    tsg_jaccard = jaccard_index(tsg_prob1, tsg_prob2,
+                                mask1=tsg_pred1, mask2=tsg_pred2)
+
+    # calc spearman rank correlation
+    sp_rho_driver, sp_pval_driver = stats.pearsonr(driver_top_rank1, driver_top_rank2)
+    sp_rho_onco, sp_pval_onco = stats.pearsonr(onco_top_rank1, onco_top_rank2)
+    sp_rho_tsg, sp_pval_tsg = stats.pearsonr(tsg_top_rank1, tsg_top_rank2)
+
+    # calc kendall tau correlation
+    kt_rho_driver, kt_pval_driver = stats.kendalltau(driver_top_rank1, driver_top_rank2)
+    kt_rho_onco, kt_pval_onco = stats.kendalltau(onco_top_rank1, onco_top_rank2)
+    kt_rho_tsg, kt_pval_tsg = stats.kendalltau(tsg_top_rank1, tsg_top_rank2)
+
+    results = pd.DataFrame({'jaccard index': [onco_jaccard,
+                                              tsg_jaccard,
+                                              driver_jaccard],
+                            'spearman correlation': [sp_rho_onco,
+                                                     sp_rho_tsg,
+                                                     sp_rho_driver],
+                            'kendall tau correlation': [kt_rho_onco,
+                                                        kt_rho_tsg,
+                                                        kt_rho_driver],
+                            'spearman p-value': [sp_pval_onco,
+                                                 sp_pval_tsg,
+                                                 sp_pval_driver],
+                            'kendall tau p-value': [kt_pval_onco,
+                                                    kt_pval_tsg,
+                                                    kt_pval_driver]},
+                            index=['oncogene', 'tsg', 'driver'])
+    return results
+
+
+def naive_bayes_compare(df1, df2, opts):
+    nbclf1 = MultinomialNaiveBayes(df1, min_ct=opts['min_count'])
+    onco_prob1, tsg_prob1, other_prob1 = nbclf1.kfold_prediction()
+    driver_prob1 = onco_prob1 + tsg_prob1
+    df1 = pd.DataFrame({'onco': onco_prob1,
+                        'tsg': tsg_prob1,
+                        'other': other_prob1,
+                        'driver': driver_prob1},
+                       index=onco_prob1.index)
+    nbclf2 = MultinomialNaiveBayes(df2, min_ct=opts['min_count'])
+    onco_prob2, tsg_prob2, other_prob2 = nbclf2.kfold_prediction()
+    driver_prob2 = onco_prob2 + tsg_prob2
+    df2 = pd.DataFrame({'onco': onco_prob2,
+                        'tsg': tsg_prob2,
+                        'other': other_prob2,
+                        'driver': driver_prob2},
+                       index=onco_prob2.index)
+
+    # get predicted class
+    onco_pred1 = (df1['onco'] > df1['tsg']) & (df1['onco'] > df1['other'])
+    onco_pred2 = (df2['onco'] > df2['tsg']) & (df2['onco'] > df2['other'])
+    tsg_pred1 = (df1['tsg'] > df1['onco']) & (df1['tsg'] > df1['other'])
+    tsg_pred2 = (df2['tsg'] > df2['onco']) & (df2['tsg'] > df2['other'])
+
+    # rank genes
+    driver_top_rank1, driver_top_rank2 = rank_genes(driver_prob1, driver_prob2, thresh=.5)
+    onco_top_rank1, onco_top_rank2 = rank_genes(onco_prob1, onco_prob2,
+                                                mask1=onco_pred1, mask2=onco_pred2)
+    tsg_top_rank1, tsg_top_rank2 = rank_genes(tsg_prob1, tsg_prob2,
+                                              mask1=tsg_pred1, mask2=tsg_pred2)
+
+    # calculate jaccard index
+    driver_jaccard = jaccard_index(driver_prob1, driver_prob2, thresh=.5)
+    onco_jaccard = jaccard_index(onco_prob1, onco_prob2,
+                                 mask1=onco_pred1, mask2=onco_pred2)
+    tsg_jaccard = jaccard_index(tsg_prob1, tsg_prob2,
+                                mask1=tsg_pred1, mask2=tsg_pred2)
+
+    # calc spearman rank correlation
+    sp_rho_driver, sp_pval_driver = stats.pearsonr(driver_top_rank1, driver_top_rank2)
+    sp_rho_onco, sp_pval_onco = stats.pearsonr(onco_top_rank1, onco_top_rank2)
+    sp_rho_tsg, sp_pval_tsg = stats.pearsonr(tsg_top_rank1, tsg_top_rank2)
+
+    # calc kendall tau correlation
+    kt_rho_driver, kt_pval_driver = stats.kendalltau(driver_top_rank1, driver_top_rank2)
+    kt_rho_onco, kt_pval_onco = stats.kendalltau(onco_top_rank1, onco_top_rank2)
+    kt_rho_tsg, kt_pval_tsg = stats.kendalltau(tsg_top_rank1, tsg_top_rank2)
+
+
+    results = pd.DataFrame({'jaccard index': [onco_jaccard,
+                                              tsg_jaccard,
+                                              driver_jaccard],
+                            'spearman correlation': [sp_rho_onco,
+                                                     sp_rho_tsg,
+                                                     sp_rho_driver],
+                            'kendall tau correlation': [kt_rho_onco,
+                                                        kt_rho_tsg,
+                                                        kt_rho_driver],
+                            'spearman p-value': [sp_pval_onco,
+                                                 sp_pval_tsg,
+                                                 sp_pval_driver],
+                            'kendall tau p-value': [kt_pval_onco,
+                                                    kt_pval_tsg,
+                                                    kt_pval_driver]},
+                            index=['oncogene', 'tsg', 'driver'])
+    return results
+
+
+def compare_prediction(df1, df2, gene_df, opts):
+    """Function called by multiprocessing to run predictions and compare
+    their predictions.
+
+    Parameters
+    ----------
+    df1 : pd.DataFrame
+        first half of the split of the gene features
+    df2 : pd.DataFrame
+        second half of the split of the gene features
+    gene_df : pd.DataFrame
+        not mutational features related to genes (e.g. gene length)
+    opts : dict
+        parameter options for classifiers
+
+    Returns
+    -------
+    tuple
+        results of predictions
+    """
+    # merge gene features
+    df1 = pd.merge(df1, gene_df,
+                   how='left', on='gene')
+    df1 = df1.set_index('gene')
+    df2 = pd.merge(df2, gene_df,
+                   how='left', on='gene')
+    df2 = df2.set_index('gene')
+
+    # run classifiers on random split
+    r_sim_results = r_random_forest_compare(df1, df2, opts)
+    py_sim_results = py_random_forest_compare(df1, df2, opts)
+    nb_sim_results = naive_bayes_compare(df1, df2, opts)
+
+    return r_sim_results, py_sim_results, nb_sim_results
+
+
+def singleprocess_compare_prediction(info):
+    dfg, gene_df, opts = info  # unpack tuple
+    df1, df2 = next(dfg.dataframe_generator())
+    single_result = compare_prediction(df1, df2, gene_df, opts)
+    return single_result
+
+
+def multiprocess_compare_prediction(dfg, gene_df, opts):
+    num_processes = opts['processes']
+    process_results = None
+
+    for i in range(0, dfg.num_iter, num_processes):
+        pool = Pool(processes=num_processes)
+        del process_results  # possibly help free up more memory
+        time.sleep(5)  # wait 5 seconds, might help make sure memory is free
+        tmp_num_pred = dfg.num_iter - i if  i + num_processes > dfg.num_iter else num_processes
+        # df_generator = dfg.dataframe_generator()
+        info_repeat = it.repeat((dfg, gene_df, opts), tmp_num_pred)
+        #pool = Pool(processes=tmp_num_pred)
+        process_results = pool.imap(singleprocess_compare_prediction, info_repeat)
+        pool.close()
+        pool.join()
+        yield process_results
+
+
+def main(cli_opts):
+    out_opts = _utils.get_output_config('feature_matrix')
+    sim_opts = _utils.get_output_config('simulate_consistency')
+    my_db_path = _utils.get_db_config('2020plus')['db']
+    conn = sqlite3.connect(my_db_path)
+
+    gene_df = sp.retrieve_gene_features(cli_opts)  # features like gene length, etc
+
+    r_result, py_result, nb_result = {}, {}, {}
+
+    # object that generates features from randomly choosen sample names while
+    # still respecting the stratification of tumor types
+    sample_rate = .5  # always do half splits
+    dfg = RandomSplit(sub_sample=sample_rate,
+                      num_iter=cli_opts['samples'],
+                      db_conn=conn)
+
+    # iterate through each sampled data set
+    r_sim_results, py_sim_results, nb_sim_results = {}, {}, {}
+    i = 0  # counter for index of data frames
+    for df_list in multiprocess_compare_prediction(dfg, gene_df, cli_opts):
+        # save all the results
+        for j, mydf in enumerate(df_list):
+            r_sim_results[i],  py_sim_results[i], nb_sim_results[i] = mydf
+            i += 1
+
+    # record result for a specific sample rate
+    tmp_results = sp.calculate_stats(r_sim_results)
+    r_result[sample_rate] = tmp_results
+    tmp_results = sp.calculate_stats(py_sim_results)
+    py_result[sample_rate] = tmp_results
+    tmp_results = sp.calculate_stats(nb_sim_results)
+    nb_result[sample_rate] = tmp_results
+
+    # make pandas panel objects out of summarized
+    # results from simulations
+    r_panel_result = pd.Panel(r_result)
+    py_panel_result = pd.Panel(py_result)
+    nb_panel_result = pd.Panel(nb_result)
+    r_path = _utils.sim_result_dir + sim_opts['r_result']
+    py_path = _utils.sim_result_dir + sim_opts['py_result']
+    nb_path = _utils.sim_result_dir + sim_opts['nb_result']
+    r_panel_result[sample_rate].to_csv(r_path, sep='\t')
+    py_panel_result[sample_rate].to_csv(py_path, sep='\t')
+    nb_panel_result[sample_rate].to_csv(nb_path, sep='\t')
+
+    # results for plotting
+    results = {'20/20+ classifier': r_panel_result[sample_rate],
+               'Random Forest': py_panel_result[sample_rate],
+               'Naive Bayes': nb_panel_result[sample_rate]}
