7bad5748b1e49112d6af535d920de25307d92c2a
ctokheim <collintokheim@gmail.com>
2016-03-17 11:56:49 -0400

    Removed unused function

--- src/utils/python/util.py
+++ src/utils/python/util.py
@@ -109,27 +109,6 @@ def keyboard_exit_wrapper(func):
     return wrap
 
 
-def read_aa_properties(file_path):
-    """Read aa property counts from the data_analysis/results folder.
-
-    Parameters
-    ----------
-    file_path : str
-        path to aa_change.properties.txt
-
-    Returns
-    -------
-    df : pd.DataFrame
-        contains mutation counts for amino acid chemical properties
-    """
-    logger = logging.getLogger(name=__name__)
-    logger.info('reading in %s ...' % file_path)
-    df = pd.read_csv(file_path, sep='\t')  # read file
-    df = df.set_index('initial_prop')  # set rows as initial property
-    logger.info('finished reading file.')
-    return df
-
-
 def read_oncogenes():
     """Reads in the oncogenes from vogelsteins' science paper.
 
