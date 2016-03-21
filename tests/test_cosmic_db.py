3f4b4e0bd7f9002e0e657db5a88b2950a7d8514a
ctokheim <collintokheim@gmail.com>
2016-03-20 20:49:38 -0400

    Removed old tests

--- tests/test_cosmic_db.py
+++
@@ -1,9 +0,0 @@
-from ..src.utils.python.cosmic_db import get_cosmic_db
-
-def test_get_cosmic_db():
-    """Test if database connection to COSMIC_nuc DB is working."""
-    conn = get_cosmic_db()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM `nucleotide` LIMIT 10")
-    print list(cursor.fetchall())
-    conn.close
