13b732938101f6390e8fb93b6281716f53342064
ctokheim <collintokheim@gmail.com>
2016-03-21 13:08:05 -0400

    Attempt to fix install

--- tests/test_run.py
+++ tests/test_run.py
@@ -5,7 +5,6 @@ file_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(file_dir, '..'))
 
 def test_run():
-    import run
     import src.classify.python.classifier
     import src.features.python.features
     import src.savedb.python.gene_tsv
